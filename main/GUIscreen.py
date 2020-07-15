9ef5ae0d54daeb32c7b8220fcccfc0cfe97a60b3
Nola Smith <smith.nola1@gmail.com>
2020-07-15 01:30:58 -0400

    Added color to the gui

--- main/GUIscreen.py
+++ main/GUIscreen.py
@@ -5,20 +5,18 @@ class GUI:
         self.quote = quote
         self.author = author
 
+    # Creates the gui screen displaying quote and author
     def create_screen(self, quote, author):
         screen = Tk()
         frame = Frame(screen)
         frame.pack()
         screen.geometry("300x125")  # controls the size of the window
-        bottomframe = Frame(screen)
-        bottomframe.pack(side=BOTTOM)
         middleframe = Frame(screen)
         middleframe.pack(side=TOP)
-        quoteLabel = Label(middleframe, text=quote, wraplength=250)
-        authorLabel = Label(middleframe, text="-"+author, wraplength=250)
+        quoteLabel = Label(middleframe, text=quote, wraplength=250, bg='#80ccff')
+        authorLabel = Label(middleframe, text="-" + author, wraplength=250, bg='#80ccff')
         quoteLabel.pack(side=TOP)
         authorLabel.pack(side=TOP)
-        randomButton = Button(bottomframe, text="New quote")
-        randomButton.place(x=50, y=50)
-        randomButton.pack(side=BOTTOM)
+        screen.configure(bg='#80ccff')
+        middleframe.configure(bg='#80ccff')
         screen.mainloop()
--- main/QuoteRetriever.py
+++ main/QuoteRetriever.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+# Retrieves quote and author from brainyquote.com
 def retrieve_from_internet():
     res = requests.get("https://www.brainyquote.com/quote_of_the_day")
     html = BeautifulSoup(res.text, 'html.parser')
@@ -9,7 +10,7 @@ def retrieve_from_internet():
     quoteWithAuthor = imgTag.select("img")
     return quoteWithAuthor[0]['alt']
 
-
+# separates and return both the quote and author as individuals
 def retrieve_quote_and_author():
     list = retrieve_from_internet().split("-")
     quote = list[0]
